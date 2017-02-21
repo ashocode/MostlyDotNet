e14145bf0bd2dd2c65e5976431cb4f23b5049421
Asho <asho@somepage.com>
2017-02-21 10:28:54 +0200

    problem one solved

---
+++ Calculation Problem/Calculation Problem/Calculation.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Calculation_Problem
+{
+    using System;
+
+    class CatsDecryption
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine().ToLower();
+            string[] splitToWords = input.Split(' ');
+
+            int value = DecodeCatsMessages(splitToWords);
+            string convertedString = ConvertToNineteen(DecodeCatsMessages(splitToWords));
+            Console.WriteLine(convertedString + " = " + value);
+
+        }
+
+        static int DecodeCatsMessages(string[] text)
+        {
+            string word = string.Empty;
+            double sum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                word = text[i];
+                for (int j = 0; j < word.Length; j++)
+                {
+                    int power = word.Length - j;
+                    int number = word[j] - 97;
+                    sum = sum + (number * Math.Pow(23, word.Length - j - 1));
+                }
+            }
+
+            int result = Convert.ToInt32(sum);
+            return result;
+        }
+
+        static string ConvertToNineteen(int number)
+        {
+            string resultStr = string.Empty;
+            if (number == 0)
+            {
+                resultStr = "a";
+            }
+
+            while (number > 0)
+            {
+                char ch = (char)((number % 23) + 'a');
+                resultStr = ch.ToString() + resultStr;
+                number /= 23;
+            }
+
+            return resultStr;
+        }
+    }
+}
+
